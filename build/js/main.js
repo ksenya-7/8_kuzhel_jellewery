7f691cfd0e644402e23f7c67dc04133c3bf8c879
Ksu <ksenya-7@yandex.ru>
2021-04-20 12:39:49 +0300

    пагинация на мобильном разрешении

--- build/js/main.js
+++ build/js/main.js
@@ -274,17 +274,29 @@ var slider = function slider() {
       // when window width is >= 1024px
       1024: {
         slidesPerView: 4,
-        spaceBetween: 30
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          createPagination: true,
+          // type: 'fraction',
+          // renderFraction(currentClass, totalClass) {
+          //   return '<span class="' + currentClass + '"></span>' + '<span class="' + totalClass + '"></span>';
+          // },
+          clickable: true
+        }
       }
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
-    } // pagination: {
-    //   el: '.swiper-pagination',
-    //   type: 'bullets',
-    // },
-
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      renderFraction: function renderFraction(currentClass, totalClass) {
+        return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+      }
+    }
   });
 };
 
--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -11,16 +11,28 @@ const slider = () => {
       1024: {
         slidesPerView: 4,
         spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          createPagination: true,
+          // type: 'fraction',
+          // renderFraction(currentClass, totalClass) {
+          //   return '<span class="' + currentClass + '"></span>' + '<span class="' + totalClass + '"></span>';
+          // },
+          clickable: true,
+        },
       },
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   type: 'bullets',
-    // },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      renderFraction(currentClass, totalClass) {
+        return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+      },
+    },
   });
 };
 
